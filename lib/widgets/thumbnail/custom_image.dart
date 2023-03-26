5e87109a5ac58e55114d889a39cdc312b2d04b16
Your Name <thanhdat5101@gmail.com>
2023-03-26 22:43:46 +0700

    hot fix

--- lib/widgets/thumbnail/custom_image.dart
+++ lib/widgets/thumbnail/custom_image.dart
@@ -6,7 +6,7 @@ import 'package:flutter/material.dart';
 
 Image customImage(String? path, {BoxFit fit = BoxFit.contain}) {
   return Image(
-    image: path == null
+    image: (path == null)
         ? customAdvanceNetworkImage(
             'https://caltech-prod.s3.amazonaws.com/main/images/CollinCamerer-ShortSelling-0.2e16d0ba.fill-1600x810-c100.jpg')
         : customAdvanceNetworkImage(path),
